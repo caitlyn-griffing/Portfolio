195f83700ba453da046680ca832967a2087a1948
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-04-26 19:50:46 -0500

    contact form and blogs page

--- app.js
+++ app.js
@@ -37,6 +37,13 @@ function PageTransitions() {
             console.log(id)
         }
     })
+
+    // Toggle Theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
 PageTransitions();
